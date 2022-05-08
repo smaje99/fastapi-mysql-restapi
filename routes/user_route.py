554b30a5306877b98b8de0b4e78771a9fa198023
Sergio Majé <smajefranco@gmail.com>
2022-05-08 18:32:07 -0500

    🚧 Create a user in route

--- routes/user_route.py
+++ routes/user_route.py
@@ -1,13 +1,35 @@
+from cryptography import Fernet
 from fastapi import APIRouter
 
 from config.database import connection as conn
-from models import User
+from models import User as model
+from schemas import User
 
+
+__key = Fernet.generate_key()
+__func = Fernet(__key)
 
 router = APIRouter()
 
 @router.get('/users')
 def get_users():
-    result = (conn.execute(User.select)
+    result = (conn.execute(model.select)
               .fetchall())
     return result
+
+@router.post('/user')
+def create_user(user: User):
+    new_user = {
+        'name': user.name,
+        'email': user.email,
+        'password': __func.encrypt(user.password.encode('utf-8')),
+    }
+
+    result = conn.execute(model.insert().values(new_user))
+    user_created = (conn.execute(
+        model
+        .select()
+        .where(model.c.id == result.lastrowid)
+    ).first())
+
+    return user_created
