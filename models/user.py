b2d8adb6d493e6b782e6d2a8664949bed8ded736
Sergio Majé <smajefranco@gmail.com>
2022-05-08 16:39:00 -0500

    🏷️ User model

---
+++ models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Table, Column
+from sqlalchemy.sql.sqltypes import Integer, String
+
+from config.database import meta, engine
+
+
+User = Table(
+    'user', meta,
+    Column('id', Integer(), primary_key=True),
+    Column('name', String(255)),
+    Column('email', String(255), unique=True),
+    Column('password', String(255)),
+)
+
+meta.create_all(engine)
