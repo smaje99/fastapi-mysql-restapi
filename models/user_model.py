aa1dc9f8104c3bf29c9bd635ebc413480234d76a
Sergio Majé <smajefranco@gmail.com>
2022-05-08 17:08:49 -0500

    ✏️ Remove a comma

--- models/user_model.py
+++ models/user_model.py
@@ -9,7 +9,7 @@ User = Table(
     Column('id', Integer(), primary_key=True),
     Column('name', String(255)),
     Column('email', String(255), unique=True),
-    Column('password', String(255)),
+    Column('password', String(255))
 )
 
 meta.create_all(engine)
