fccb024433dda730323a386938caf99d29956431
Sergio Majé <smajefranco@gmail.com>
2022-05-08 15:38:16 -0500

    🔧 Connection options to the database

--- config/env.py
+++ config/env.py
@@ -8,3 +8,12 @@ __path = os.path.join(__dir, '..', '.env')
 __dotenv_path = Path(__path).resolve()
 
 load_dotenv(dotenv_path=__dotenv_path)
+
+
+ConnectionDBOptions = {
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT'),
+    'user': os.getenv('DB_UID'),
+    'password': os.getenv('DB_PWD'),
+    'database': os.getenv('DB_DB')
+}
